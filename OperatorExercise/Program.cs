b997047521f20b1721ddb67ef0a4cf17a933a1dd
JustinBenning <justin.benning@gmail.com>
2024-01-15 14:57:03 -0500

    upload of project

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace OperatorExercise
+﻿using System.Transactions;
+
+namespace OperatorExercise
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           /* int a = 17;
+            int b = 4;
+
+            int addSum = a + b;
+            int diffSum = a - b;
+            int multSum = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");*/
+
+
+
+            Console.WriteLine("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area of a circle:" + AreaOfCircle(radius));
+            
+
+           
+
+
+
         }
-    }
-}
+        public static double AreaOfCircle(double radius)
+        {
+
+            return Math.PI * Math.Pow(radius, 2);
+            
+           
+        }
+    } }
